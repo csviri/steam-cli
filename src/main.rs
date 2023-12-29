8d0a3fe9427478c0f36ab8ba0a020f9784fe6114
Attila Mészáros <csviri@gmail.com>
2023-12-29 19:19:01 +0100

    wip
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+use std::env;
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let name = "STEAM_API_KEY";
+    let api_key;
+    match env::var(name) {
+        Ok(v) => api_key = v,
+        Err(e) => panic!("${} is not set", name)
+    }
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        panic!("At least 2 user IDs needed")
+    }
+    let first = args.get(0).unwrap();
+
+    let url = format!("https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={first}&format=json");
+
+    println!("url: {}", url);
+
+    let resp = reqwest::get(url)
+        .await?
+        .json::<serde_json::Value>()
+        .await?;
+
+    println!("{:#?}", resp);
+    Ok(())
 }
+
