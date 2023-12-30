aea3632b225214a2f870fca4dfe804e2d256ed28
Attila Mészáros <csviri@gmail.com>
2023-12-30 19:03:05 +0100

    working
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use std::collections::HashSet;
+use serde_json::Value;
+use std::collections::{HashMap, HashSet};
 use std::env;
+use std::env::temp_dir;
+use tokio::fs;
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -7,18 +12,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let api_key = match env::var(name) {
         Ok(v) => v,
-        Err(_) => panic!("${} is not set", name),
+        Err(_) => panic!("${} env var is not set", name),
     };
 
     let mut args: Vec<String> = env::args().collect();
     args.remove(0);
 
-    // if args.len() < 2 {
-    //     panic!("At least 2 user IDs needed")
-    // }
+    if args.is_empty() {
+        panic!("At least 1 user ID needed")
+    }
 
     let mut app_ids = HashSet::new();
-
     for user_id in args {
         let actual_ids = get_game_ids(user_id, api_key.clone()).await?;
         if app_ids.is_empty() {
@@ -28,8 +32,59 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    println!("{:#?}", app_ids);
+    let names = app_ids_to_names(app_ids).await?;
+
+    println!("Common games: {:#?}", names);
     Ok(())
+}
+
+async fn app_ids_to_names(
+    app_ids: HashSet<i64>,
+) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let mut mapping_file = temp_dir();
+    mapping_file.push("appid_to_names.json");
+    println!("tmp mapping file: {}", mapping_file.to_str().unwrap());
+
+    if !mapping_file.is_file() {
+        println!("NOT Found appid to name file. Downloading it");
+        let contents = reqwest::get("https://api.steampowered.com/ISteamApps/GetAppList/v2/")
+            .await?
+            .text()
+            .await?;
+        let mut created = File::create(mapping_file.clone().as_path()).await?;
+        created.write_all(contents.as_bytes()).await?;
+        created.sync_all().await?;
+    } else {
+        println!("Found appid to name mapping file.")
+    }
+    let contents = fs::read(mapping_file).await?;
+    let value: Value = serde_json::from_slice(contents.as_slice()).unwrap();
+    let vals = value
+        .get("applist")
+        .unwrap()
+        .get("apps")
+        .unwrap()
+        .as_array()
+        .unwrap();
+    let mut map = HashMap::new();
+    for v in vals {
+        map.insert(
+            v.get("appid").unwrap().as_i64().unwrap(),
+            String::from(v.get("name").unwrap().as_str().unwrap()),
+        );
+    }
+
+    let mut result = Vec::new();
+    for app_id in app_ids {
+        let name = map.get(&app_id);
+        if name.is_none() {
+            println!("Cannot find name for id: {}", app_id)
+        } else {
+            result.push(name.unwrap().clone());
+        }
+    }
+
+    return Ok(result);
 }
 
 async fn get_game_ids(
